26491815c45f16ae638007a606f0036cbad7f06d
Casaubon <netzach.yoshida@gmail.com>
2021-02-22 21:01:05 +1300

    feat: 🎸 id component

---
+++ pages/p/[id].tsx
@@ -0,0 +1,73 @@
+import { GetServerSideProps } from 'next'
+import React from 'react'
+import ReactMarkdown from 'react-markdown'
+import styled from 'styled-components'
+import Layout from '../../components/Layout'
+import { PostProps } from '../../components/Post'
+import prisma from '../../lib/prisma'
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const post = await prisma.post.findUnique({
+    where: {
+      id: Number(params?.id) || -1,
+    },
+    include: {
+      author: {
+        select: { name: true },
+      },
+    },
+  })
+
+  return {
+    props: { post },
+  }
+}
+
+type Props = {flag: boolean} & PostProps
+
+const Component: React.FC<Props> = props => (
+    <Layout>
+      <div>
+        <h2>{props.flag ? `${props.title} (Draft)` : props.title}</h2>
+        <p>By {props?.author?.name || "Unknown author"}</p>
+        {/* I changed \"source\" as an element of ReactMarkdown because of deprecated */}
+        <ReactMarkdown children={props.content} />
+      </div>
+    </Layout>
+  )
+
+const StyledComponent = styled(Component)`
+  .page {
+    background: white;
+    padding: 2rem;
+  }
+
+  .actions {
+    margin-top: 2rem;
+  }
+
+  button {
+    background: #ececec;
+    border:0;
+    border-radius: 0.125rem;
+    padding: 1rem 2rem;
+  }
+
+  button + button {
+    margin-left: 1rem;
+  }
+`
+
+type ContainerProps = PostProps
+
+const Container: React.FC<ContainerProps> = props => {
+  const flag = !props.published
+  return (
+    <StyledComponent
+      {...props}
+      flag={flag}
+    />
+  )
+}
+
+export default Container
