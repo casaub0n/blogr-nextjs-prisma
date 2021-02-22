5f651cb83ccaf8519f351c6e5c8406ebc34fba62
Casaubon <netzach.yoshida@gmail.com>
2021-02-22 21:00:27 +1300

    fix: 🐛 layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,22 +1,62 @@
 import * as React from 'react'
-import Head from 'next/head'
+import styled, { createGlobalStyle } from 'styled-components'
+import Header from './Header'
 
-type Props = {
-  title?: string
-}
+/**
+ * TODO: not good
+ */
+const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
 
-const Layout: React.FC<Props> = ({
-  children,
-  title = 'This is the default title',
-}) => (
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    font-size: 16px;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+      Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
+      "Segoe UI Symbol";
+    background-color: rgba(0, 0, 0, 0.05);
+  }
+
+  input,
+  textarea {
+    font-size: 16px;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`
+
+const Component: React.FC = props => (
   <div>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    {children}
+    <Header isActive={true} />
+    <div className="layout">{props.children}</div>
+    <GlobalStyle />
   </div>
 )
 
-export default Layout
+const StyledComponent = styled(Component)`
+  .layout {
+    padding: 0.2rem;
+  }
+`
+
+const Container: React.FC = props => {
+  return (
+    <StyledComponent
+      {...props}
+      children={props.children}
+    />
+  )
+}
+
+export default Container
