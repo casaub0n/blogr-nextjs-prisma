159112f2e98d283cadbc8811a4adb0c68963a52c
Casaubon <netzach.yoshida@gmail.com>
2021-05-09 22:58:22 +1200

    feat: 🎸 add div

--- components/DogImage.tsx
+++ components/DogImage.tsx
@@ -8,8 +8,8 @@ export type IProps = {
 
 const Component: React.VFC<IProps> = props => {
   return (
-    <div className="w-full md:w-1/2 lg:w-1/3 flex flex-col mb-8 px-3">
-      <div className="overflow-hidden bg-white rounded-lg shadow hover:shadow-raised hover:translateY-2px transition">
+      <div className="w-full md:w-1/2 lg:w-1/3 flex flex-col mb-8 px-3">
+        <div className="overflow-hidden bg-white rounded-lg shadow hover:shadow-raised hover:translateY-2px transition">
           <img className="dogimage w-full" src={props.src} alt="cute dog!" />
             <div className="p-6 flex flex-col justify-between">
               <h3 className="font-medium text-gray-900 mb-4 leading-normal">Awesome Shiba dooog!!!</h3>
