f1bd3071e523167dd6c6886a1c876a57a05c7c82
Casaubon <netzach.yoshida@gmail.com>
2021-05-23 17:03:02 +1200

    feat: 🎸 remove componet

--- components/header/Header.stories.tsx
+++
@@ -1,7 +0,0 @@
-import Link from 'next/link'
-import React from 'react'
-import { StyledComponent as Component } from './atoms/Bold'
-
-export default { title: 'Bold button' }
-
-export const BoldLink: React.VFC = () => <Link href="/"><Component className="bold">Feed</Component></Link>
