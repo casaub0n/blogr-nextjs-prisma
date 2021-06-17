08ff505baf74ac257523cbbf19f35fc742ea5181
Casaubon <netzach.yoshida@gmail.com>
2021-06-17 23:46:23 +1200

    fix: 🐛 stories

--- components/Post.stories.tsx
+++ components/Post.stories.tsx
@@ -1,11 +1,18 @@
+import { Meta, Story } from '@storybook/react'
 import React, { ComponentProps } from 'react'
-import { StyledComponent as Component } from '../components/Post'
+import { StyledComponent as Component } from './Post'
 
-export default { title: 'Post' }
+export default {
+  title: 'Post',
+  component: Component
+} as Meta
 
 type Props = ComponentProps<typeof Component>
 
-const meta: Props = {
+const Template: Story<Props> = args => <Component {...args} />
+
+export const Default = Template.bind({})
+Default.args = {
   id: 1999,
   title: 'Example title',
   author: {
@@ -17,4 +24,3 @@ const meta: Props = {
   className: 'storyPost'
 }
 
-export const Post: React.VFC = () => <Component {...meta} />
