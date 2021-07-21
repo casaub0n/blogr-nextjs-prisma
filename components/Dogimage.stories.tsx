bdd20ea73fc9135ab5cc37751bac7fcb9720cf2b
Casaubon <netzach.yoshida@gmail.com>
2021-07-21 16:57:54 +1200

    fix: 🐛 storybook

--- components/Dogimage.stories.tsx
+++ components/Dogimage.stories.tsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import { Story } from '@storybook/react'
+import React, { ComponentProps } from 'react'
 import { StyledComponent as Component } from '../components/DogImage'
 
 export default { title: 'Dog Image' }
 
-export const withDogImage = <Component src="https://images.dog.ceo/breeds/shiba/shiba-8.jpg" />
+// export const withDogImage = <Component src="https://images.dog.ceo/breeds/shiba/shiba-8.jpg" />
+
+type Props = ComponentProps<typeof Component>
+
+const Template: Story<Props> = args => <Component {...args} />
+
+export const Default = Template.bind({})
+Default.args = {
+  src: 'https://images.dog.ceo/breeds/shiba/shiba-8.jpg'
+}
