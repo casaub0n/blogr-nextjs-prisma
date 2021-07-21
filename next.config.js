8f5cef5c42f5dec316f4820c2d68962f8aae6717
Casaubon <netzach.yoshida@gmail.com>
2021-07-21 15:47:18 +1200

    feat: 🎸 update config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
-module.exports = {
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.node = { fs: 'empty', module: 'empty'}
-    }
+// ValidationError: Invalid configuration object. Webpack has been initialized using a configuration object that does not match the API schema.
+// module.exports = {
+//   webpack: (config, { isServer }) => {
+//     // Fixes npm packages that depend on `fs` module
+//     if (!isServer) {
+//       config.node = { fs: 'empty', module: 'empty'}
+//     }
 
-    return config
-  },
-}
+//     return config
+//   },
+// }
