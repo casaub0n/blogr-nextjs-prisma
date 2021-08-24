c6c62fda31f9a1ff78ed7ebef18b9526510d632e
Casaubon <netzach.yoshida@gmail.com>
2021-08-24 19:52:20 +1200

    feat: 🎸 implement feature for draft post

---
+++ components/Draft.stories.tsx
@@ -0,0 +1,17 @@
+import React, { ComponentProps } from 'react'
+import { Meta, Story } from '@storybook/react'
+import { StyledComponent as Component } from './Draft'
+
+export default {
+  title: 'Create post',
+  component: Component,
+} as Meta
+
+type Props = ComponentProps<typeof Component>
+
+const Template: Story<Props> = args => <Component {...args} />
+export const Default = Template.bind({})
+Default.args = {
+  className: "page",
+  login: false
+}
