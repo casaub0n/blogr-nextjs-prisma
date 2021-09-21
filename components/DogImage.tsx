4d13b9b3d0b6e17dae8d62d5fed60016e0786310
Casaubon <netzach.yoshida@gmail.com>
2021-09-21 22:55:56 +1200

    refactor: 💡 use next/image

--- components/DogImage.tsx
+++ components/DogImage.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import Image from 'next/image'
 
 export type IProps = {
   src: string
@@ -11,7 +12,7 @@ const Component: React.VFC<IProps> = props => {
     <div className={props.className}>
       <div className="w-full md:w-1/2 lg:w-1/3 flex flex-col mb-8 px-3">
         <div className="overflow-hidden bg-white rounded-lg shadow hover:shadow-raised hover:translateY-2px transition">
-          <img className="dogimage w-full" src={props.src} alt="cute dog!" />
+          <Image className="dogimage w-full" src={props.src} alt="cute dog!" />
             <div className="p-6 flex flex-col justify-between">
               <h3 className="font-medium text-gray-900 mb-4 leading-normal">Awesome Shiba dooog!!!</h3>
               <p className="inline-flex items-center">
