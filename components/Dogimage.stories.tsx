33b675a1d159902de221bdc9330e3bb55a1980b2
Casaubon <netzach.yoshida@gmail.com>
2021-09-22 13:14:09 +1200

    refactor: 💡 ignore for storybook

--- components/Dogimage.stories.tsx
+++ components/Dogimage.stories.tsx
@@ -2,6 +2,7 @@ import { Story } from '@storybook/react'
 import React, { ComponentProps } from 'react'
 import { StyledComponent as Component } from '../components/DogImage'
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default { title: 'Dog Image' }
 
 // export const withDogImage = <Component src="https://images.dog.ceo/breeds/shiba/shiba-8.jpg" />
--- components/Me.stories.tsx
+++ components/Me.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Me, MeAvatar } from '../components/Me'
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default { title: 'Test for styled-component' }
 
 export const withMe = <Me className="hoge" />
