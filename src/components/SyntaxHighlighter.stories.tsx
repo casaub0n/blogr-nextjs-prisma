af71047e2782c9cdf60e0fa50d2592db26b2884d
Casaubon <netzach.yoshida@gmail.com>
2021-10-12 14:08:52 +1300

    feat: 🎸 add syntax highlighter

---
+++ src/components/SyntaxHighlighter.stories.tsx
@@ -0,0 +1,19 @@
+import { Meta, Story } from '@storybook/react'
+import React, { ComponentProps } from 'react'
+import { Component } from './SyntaxHighlighter'
+
+export default {
+  title: 'Syntex highligh',
+  component: Component
+} as Meta
+
+type Props = ComponentProps<typeof Component>
+
+const Template: Story<Props> = args => <Component {...args} />
+
+export const Default = Template.bind({})
+Default.args = {
+  codeString: 'export const Component: React.VFC = () => <div>Hello world</div>',
+  language: 'javascript'
+}
+
