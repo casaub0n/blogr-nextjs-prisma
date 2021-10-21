41c2929f97d1d427ead082acb26201cbb03cbf31
Casaubon <netzach.yoshida@gmail.com>
2021-10-21 17:13:47 +1300

    fix: 🐛 adjust for typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,11 @@
-// eslint-disable-next-line no-undef
 module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  "ignorePatterns": ["!.eslintrc.js", "!.babelrc.js"],
   "extends": [
     "next",
-    "next/core-web-vitals"
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended",
+    "prettier"
   ]
 }
