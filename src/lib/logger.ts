9abdc2497d5322042d1d6110b42eb0fa31195044
Casaubon <netzach.yoshida@gmail.com>
2021-10-21 19:03:43 +1300

    feat: 🎸 TODO:VERCEL_GITHUB_COMMIT_SHA

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -17,7 +17,8 @@ const logger = pino({
   level: "debug",
   base: {
     env: process.env.NODE_ENV,
-    revision: process.env.VERCEL_GITHUB_COMMIT_SHA,
+    // revision: process.env.VERCEL_GITHUB_COMMIT_SHA,
+    revision: '',
   },
 }, stream)
 
