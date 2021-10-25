36d798aae7f835f13d6159e6fd9b318deb645dfd
Casaubon <netzach.yoshida@gmail.com>
2021-10-25 23:48:31 +1300

    feat: 🎸 add clsx

--- src/components/DogImage.tsx
+++ src/components/DogImage.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import styled from 'styled-components'
+import clsx from 'clsx'
 
 export type IProps = {
   src: string
@@ -10,13 +11,18 @@ export type IProps = {
 const Component: React.VFC<IProps> = props => {
   return (
     <div className={props.className}>
-      <div className={`w-full md:w-1/2 lg:w-1/3 flex flex-col mb-8 px-3`}>
-        <div className={`overflow-hidden bg-white rounded-lg shadow hover:shadow-raised hover:translateY-2px transition`}>
-          <Image className={`dogimage w-full`} src={props.src} alt="cute dog!" />
-            <div className={`p-6 flex flex-col justify-between`}>
-              <h3 className={`font-medium text-gray-900 mb-4 leading-normal`}>Awesome Shiba dooog!!!</h3>
-              <p className={`inline-flex items-center`}>
-                <span className={`text-gray-700 text-sm`}>Read More</span>
+      <div className={clsx(
+        'w-full',
+        'md:w-1/2 lg:w-1/3',
+        'flex flex-col',
+        'mb-8 px-3'
+      )}>
+        <div className={clsx('overflow-hidden bg-white rounded-lg shadow hover:shadow-raised hover:translateY-2px transition')}>
+          <Image className={clsx('dogimage w-full')} src={props.src} alt="cute dog!" />
+            <div className={clsx('p-6 flex flex-col justify-between')}>
+              <h3 className={clsx('font-medium text-gray-900 mb-4 leading-normal')}>Awesome Shiba dooog!!!</h3>
+              <p className={clsx('inline-flex items-center')}>
+                <span className={clsx('text-gray-700 text-sm')}>Read More</span>
               </p>
             </div>
         </div>
